0ea290497f4336506c18ef32d29c24f0183c1447
Dominique Mitchell Seraphin <dseraphin3326@gmail.com>
2016-10-18 11:36:26 -0700

    basic css layout and js module

--- public/js/app.js
+++ public/js/app.js
@@ -2,3 +2,23 @@
  * app.js, code for Trumphouse game.
  */
 
+
+
+var game = ( function() {
+	var trump ={}; //object
+
+	var bricks = []; //arrays
+
+	var arena = {}; // object, game canvas
+
+	return {
+
+		trump: trump,
+		bricks: bricks,
+		arena: arena
+
+	};
+
+}) (); //then () cuasees this function to execute instantly
+
+console.log('in app.js');
