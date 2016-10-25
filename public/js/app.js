bfb86a431ac2906122aa2ee2f3275284466ba4a3
student <student@ai251lab0216m14.aicala.edmc.stu>
2016-10-25 11:11:18 -0700

    week 4 init and controller

--- public/js/app.js
+++ public/js/app.js
@@ -11,11 +11,122 @@ var game = ( function() {
 
 	var arena = {}; // object, game canvas
 
+	var whiteHouse = {}; //win zone
+
+	function initModel() {
+		console.log('in initModel');
+
+		//init trump
+		trump.dom = document.getElementById('trump');
+		trump.name = trump.dom.id;
+
+		arena.dom = document.getElementById('game-canvas');
+		arena.name= arena.dom.id;
+
+		//init bricks
+		var list = document.getElementsByClassName('bricks');
+
+		for (var i = 0; i < list.length; i++) {
+
+			bricks.push(
+
+				{
+					dom: list[i],
+
+					name: list[i].id
+				}
+
+			);
+			
+		}//end of loop
+
+	}
+
+	function initView() {
+		console.log('in initView');
+
+	}
+
+
+	//main gameloop
+	function gameLoop() {
+		console.log('in gameLoop');
+	}
+
+	//function to check if keys are pressed
+	function checkKey(e) {
+		e = e || window.event;
+
+		if(e.keyCode == '38'){
+			console.log('up arrow pressed');
+		}
+		else if(e.keyCode == '40'){
+			console.log('down arrow pressed');
+		}
+	}
+
+	var animateId = null;
+
+	//init the controller
+	function initController() {
+		console.log('in initController');
+
+		
+
+		document.onkeydown = checkKey;
+
+	}
+
+	function start() {
+		if ( animateId == null) {
+			animateId = setInterval(
+				function() {
+
+					gameLoop();
+
+				}, 100
+
+			);
+
+			console.log('starting game');
+		}
+	}
+
+	function stop() {
+
+		clearInterval(animateId);
+
+		animateId = null;
+		console.log('stop');
+	}
+
+	function isStarted() {
+		if( animateId !==null ) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	function init() {
+		console.log('in Init');
+		initModel();
+		initView();
+		initController();
+		start();
+
+	}
+
 	return {
 
 		trump: trump,
 		bricks: bricks,
-		arena: arena
+		arena: arena,
+		whiteHouse: whiteHouse,
+		init: init,
+		start: start,
+		stop: stop
 
 	};
 
